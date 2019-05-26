12e75cae4f9ebebc365ad19f1015cfe9bb4cbc52
Colin Jones <trptcolin@gmail.com>
2019-05-26 11:00:06 -0500

    Display the slides on clicks and wrap around

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,21 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 import CarouselButton from "./CarouselButton";
+import CarouselSlide from "./CarouselSlide";
 
 class Carousel extends React.PureComponent {
+  static propTypes = {
+    slides: PropTypes.arrayOf(PropTypes.share(CarouselSlide.propTypes))
+      .isRequired,
+  };
   state = {
     slideIndex: 0,
   };
 
+  handleWrapping = index => {
+    const { slides } = this.props;
+    return (index + slides.length) % slides.length;
+  };
+
   handleClickPrevious = () => {
-    this.setState(({ slideIndex }) => ({ slideIndex: slideIndex - 1 }));
+    this.setState(({ slideIndex }) => ({
+      slideIndex: this.handleWrapping(slideIndex - 1),
+    }));
   };
   handleClickNext = () => {
-    this.setState(({ slideIndex }) => ({ slideIndex: slideIndex + 1 }));
+    this.setState(({ slideIndex }) => ({
+      slideIndex: this.handleWrapping(slideIndex + 1),
+    }));
   };
 
   render() {
+    const { slides, ...rest } = this.props;
     return (
-      <div>
+      <div {...rest}>
+        <CarouselSlide {...slides[this.state.slideIndex]} />
         <CarouselButton data-action="prev" onClick={this.handleClickPrevious}>
           Previous
         </CarouselButton>
--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -2,12 +2,30 @@ import React from "react";
 import { shallow } from "enzyme";
 import Carousel from "../Carousel";
 import CarouselButton from "../CarouselButton";
+import CarouselSlide from "../CarouselSlide";
 
 describe("Carousel", () => {
   let wrapper;
+  const slides = [
+    {
+      imgUrl: "https://example.com/slide1.png",
+      description: "Slide 1",
+      attribution: "Uno Pizzeria",
+    },
+    {
+      imgUrl: "https://example.com/slide2.png",
+      description: "Slide 2",
+      attribution: "Dos Equis",
+    },
+    {
+      imgUrl: "https://example.com/slide3.png",
+      description: "Slide 3",
+      attribution: "Three Amigos",
+    },
+  ];
 
   beforeEach(() => {
-    wrapper = shallow(<Carousel />);
+    wrapper = shallow(<Carousel slides={slides} />);
   });
 
   it("renders a <div>", () => {
@@ -36,15 +54,50 @@ describe("Carousel", () => {
     ).toBe("Next");
   });
 
-  it("decrements `slideIndex` when Previous is clicked", () => {
-    wrapper.setState({ slideIndex: 1 });
-    wrapper.find('[data-action="prev"]').simulate("click");
-    expect(wrapper.state("slideIndex")).toBe(0);
+  describe("with a middle slide selected", () => {
+    beforeEach(() => {
+      wrapper.setState({ slideIndex: 1 });
+    });
+
+    it("decrements `slideIndex` when Previous is clicked", () => {
+      wrapper.find('[data-action="prev"]').simulate("click");
+      expect(wrapper.state("slideIndex")).toBe(0);
+    });
+
+    it("increments `slideIndex` when Next is clicked", () => {
+      wrapper.find('[data-action="next"]').simulate("click");
+      expect(wrapper.state("slideIndex")).toBe(2);
+    });
   });
 
-  it("increments `slideIndex` when Next is clicked", () => {
+  describe("with the first slide selected", () => {
+    beforeEach(() => {
+      wrapper.setState({ slideIndex: 0 });
+    });
+
+    it("wraps `slideIndex` to the end when Previous is clicked", () => {
+      wrapper.find('[data-action="prev"]').simulate("click");
+      expect(wrapper.state("slideIndex")).toBe(slides.length - 1);
+    });
+  });
+
+  describe("with the last slide selected", () => {
+    beforeEach(() => {
+      wrapper.setState({ slideIndex: slides.length - 1 });
+    });
+
+    it("wraps `slideIndex` to the start when Next is clicked", () => {
+      wrapper.find('[data-action="next"]').simulate("click");
+      expect(wrapper.state("slideIndex")).toBe(0);
+    });
+  });
+
+  it("renders the current slide as a CarouselSlide", () => {
+    let slideProps;
+    slideProps = wrapper.find(CarouselSlide).props();
+    expect(slideProps).toEqual(slides[0]);
     wrapper.setState({ slideIndex: 1 });
-    wrapper.find('[data-action="next"]').simulate("click");
-    expect(wrapper.state("slideIndex")).toBe(2);
+    slideProps = wrapper.find(CarouselSlide).props();
+    expect(slideProps).toEqual(slides[1]);
   });
 });
