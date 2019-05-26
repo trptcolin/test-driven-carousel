ef6b5fe71ab04a72c1e0ebfc9044298512690e19
Colin Jones <trptcolin@gmail.com>
2019-05-26 10:44:25 -0500

    Update carousel state on clicks

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,12 +1,29 @@
 import React from "react";
+import CarouselButton from "./CarouselButton";
 
 class Carousel extends React.PureComponent {
   state = {
     slideIndex: 0,
   };
 
+  handleClickPrevious = () => {
+    this.setState(({ slideIndex }) => ({ slideIndex: slideIndex - 1 }));
+  };
+  handleClickNext = () => {
+    this.setState(({ slideIndex }) => ({ slideIndex: slideIndex + 1 }));
+  };
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <CarouselButton data-action="prev" onClick={this.handleClickPrevious}>
+          Previous
+        </CarouselButton>
+        <CarouselButton data-action="next" onClick={this.handleClickNext}>
+          Next
+        </CarouselButton>
+      </div>
+    );
   }
 }
 
--- src/tests/Carousel.test.js
+++ src/tests/Carousel.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Carousel from "../Carousel";
+import CarouselButton from "../CarouselButton";
 
 describe("Carousel", () => {
   let wrapper;
@@ -15,5 +16,35 @@ describe("Carousel", () => {
 
   it("has an initial `slideIndex` of 0", () => {
     expect(wrapper.state("slideIndex")).toBe(0);
+  });
+
+  it('renders a CarouselButton labeled "Previous"', () => {
+    expect(
+      wrapper
+        .find(CarouselButton)
+        .at(0)
+        .prop("children")
+    ).toBe("Previous");
+  });
+
+  it('renders a CarouselButton labeled "Next"', () => {
+    expect(
+      wrapper
+        .find(CarouselButton)
+        .at(1)
+        .prop("children")
+    ).toBe("Next");
+  });
+
+  it("decrements `slideIndex` when Previous is clicked", () => {
+    wrapper.setState({ slideIndex: 1 });
+    wrapper.find('[data-action="prev"]').simulate("click");
+    expect(wrapper.state("slideIndex")).toBe(0);
+  });
+
+  it("increments `slideIndex` when Next is clicked", () => {
+    wrapper.setState({ slideIndex: 1 });
+    wrapper.find('[data-action="next"]').simulate("click");
+    expect(wrapper.state("slideIndex")).toBe(2);
   });
 });
