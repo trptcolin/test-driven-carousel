fb9a9e274395d1eced95285c65386a313394ecc9
Colin Jones <trptcolin@gmail.com>
2019-05-26 13:31:23 -0500

    Add styled-components & stylelint

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,9 +5,15 @@ import CarouselSlide from "./CarouselSlide";
 
 class Carousel extends React.PureComponent {
   static propTypes = {
+    defaultImgHeight: CarouselSlide.propTypes.imgHeight,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
       .isRequired,
   };
+
+  static defaultProps = {
+    defaultImgHeight: CarouselSlide.defaultProps.imgHeight,
+  };
+
   state = {
     slideIndex: 0,
   };
@@ -29,10 +35,13 @@ class Carousel extends React.PureComponent {
   };
 
   render() {
-    const { slides, ...rest } = this.props;
+    const { defaultImgHeight, slides, ...rest } = this.props;
     return (
       <div {...rest}>
-        <CarouselSlide {...slides[this.state.slideIndex]} />
+        <CarouselSlide
+          imgHeight={defaultImgHeight}
+          {...slides[this.state.slideIndex]}
+        />
         <CarouselButton data-action="prev" onClick={this.handleClickPrevious}>
           Previous
         </CarouselButton>
--- src/CarouselSlide.js
+++ src/CarouselSlide.js
@@ -1,9 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
-const CarouselSlide = ({ imgUrl, description, attribution, ...rest }) => (
+const Img = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: ${props =>
+    typeof props.imgHeight === "number"
+      ? `${props.imgHeight}px`
+      : props.imgHeight};
+`;
+
+const CarouselSlide = ({
+  imgUrl,
+  imgHeight,
+  description,
+  attribution,
+  ...rest
+}) => (
   <figure {...rest}>
-    <img src={imgUrl} />
+    <Img src={imgUrl} imgHeight={imgHeight} />
     <figcaption>
       <strong>{description}</strong> {attribution}
     </figcaption>
@@ -11,7 +27,11 @@ const CarouselSlide = ({ imgUrl, description, attribution, ...rest }) => (
 );
 CarouselSlide.propTypes = {
   imgUrl: PropTypes.string.isRequired,
+  imgHeight: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   description: PropTypes.node.isRequired,
   attribution: PropTypes.node,
+};
+CarouselSlide.defaultProps = {
+  imgHeight: 500,
 };
 export default CarouselSlide;
