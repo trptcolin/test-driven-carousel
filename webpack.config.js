affeaba13de981bb3a6becd927cb788d32ec1ef9
Colin Jones <trptcolin@gmail.com>
2019-05-26 12:54:34 -0500

    Get browser build working

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: {
     carousel: "./src/Carousel.js",
+    example: "./example/index.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Carousel Example",
+      chunks: ["example"],
+    }),
+  ],
   module: {
     rules: [
       {
