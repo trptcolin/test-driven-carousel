8c20ca0f1440e6207f584f2e69ee6f551129340a
Colin Jones <trptcolin@gmail.com>
2019-05-26 09:46:56 -0500

    Pass props down to button

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -6,8 +6,28 @@ import CarouselButton from "../CarouselButton";
 configure({ adapter: new Adapter() });
 
 describe("CarouselButton", () => {
+  const text = "button text here";
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+  });
+
   it("renders a button", () => {
-    const wrapper = shallow(<CarouselButton />);
     expect(wrapper.type()).toBe("button");
+  });
+
+  it("allows children", () => {
+    expect(wrapper.prop("children")).toBe(text);
+  });
+
+  it("passes through other props to the <button>", () => {
+    const onClick = () => {};
+    const className = "my-favorite-carousel-button";
+    const dataAction = "prev";
+    wrapper.setProps({ onClick, className, "data-action": dataAction });
+    expect(wrapper.prop("onClick")).toBe(onClick);
+    expect(wrapper.prop("className")).toBe(className);
+    expect(wrapper.prop("data-action")).toBe(dataAction);
   });
 });
